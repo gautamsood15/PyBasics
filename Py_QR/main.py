43352d6dc00e7d28f1204f5597941f2ea701ee38
Gautam Sood <gauty22@gmail.com>
2021-03-29 20:26:16 +0530

    QR generator completed

--- Py_QR/main.py
+++ Py_QR/main.py
@@ -1,8 +1,8 @@
 import pyqrcode
 import os, shutil
 
-title = input("Give your QR code a title! >>")
-text = input("What would you like your QR code to say?")
+title = input("Give your QR code a title! >> ")
+text = input("What would you like your QR code to say? ")
 
 file_name_svg = title + ".svg"
 file_name_png = title + ".png"
@@ -12,10 +12,10 @@ url = pyqrcode.create(text)
 url.svg(file_name_svg, scale=8)
 url.png(file_name_svg, scale=10)
 
-os.mkdir(fr"/home/gautam/Desktop/Py_Basics/Py_QR/qrcodefile/{title}")
+os.mkdir(fr"/home/gautam/Desktop/Py_Basics/Py_QR/{title}")
 
-shutil.move(f"{file_name_png}", fr"/home/gautam/Desktop/Py_Basics/Py_QR/qrcodefile/{title}")
-shutil.move(f"{file_name_svg}", fr"/home/gautam/Desktop/Py_Basics/Py_QR/qrcodefile/{title}")
+shutil.move(f"{file_name_png}", fr"/home/gautam/Desktop/Py_Basics/Py_QR/{title}")
+shutil.move(f"{file_name_svg}", fr"/home/gautam/Desktop/Py_Basics/Py_QR/{title}")
 
 
 
