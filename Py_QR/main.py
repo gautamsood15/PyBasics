960d2129daea13e14c6dfb76e21b4db63ddec002
Gautam Sood <gauty22@gmail.com>
2021-03-30 20:12:04 +0530

    Adding code for data input

--- Py_QR/main.py
+++ Py_QR/main.py
@@ -1,1 +1,13 @@
+import pyqrcode
+import os, shutil
+
+title = input("Give your QR code a title! >>")
+text = input("What would you like your QR code to say?")
+
+file_name_svg = title + ".svg"
+file_name_svg = title + ".png"
+
+
+
+
 
