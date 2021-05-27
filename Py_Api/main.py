9a62414eac04ab1992300bbcccdb8c76d5b4efb1
Gautam Sood <gauty22@gmail.com>
2021-05-27 09:56:02 +0530

    Creator Json dump for home page

--- Py_Api/main.py
+++ Py_Api/main.py
@@ -1,4 +1,14 @@
 from flask import *
 import json, time
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+def home_page():
+    data_set = {'Page': 'Home', 'Message': 'Successfully loaded the Home Page', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
 
