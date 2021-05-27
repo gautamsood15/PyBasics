a55bc74cad6284118154bf6f285d3ba3603868f7
Gautam Sood <gauty22@gmail.com>
2021-05-27 10:00:07 +0530

    Adding json dump for request page

--- Py_Api/main.py
+++ Py_Api/main.py
@@ -12,3 +12,13 @@ def home_page():
     return json_dump
 
 
+@app.route('/user/', methods=['GET'])
+
+def request_page():
+
+    user_query = str(request.args.get('user')) #    /user/?user=USER_NAME
+    data_set = {'Page': 'Request', 'Message': f'Successfully got the request for {user_query}', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+
+    return json_dump
+
