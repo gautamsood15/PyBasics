cff8d9d50124904d710301f70422be888b788ce7
Gautam Sood <gauty22@gmail.com>
2021-05-30 11:53:17 +0530

    Adding try/exception and create table command

--- Py_DBConnect/main.py
+++ Py_DBConnect/main.py
@@ -3,3 +3,10 @@ import sys
 
 connection = sqlite3.connect("people.db")
 cursor = connection.cursor()
+
+try:
+    cursor.execute("CREATE TABLE people (name TEXT, age INTEGER, skills STRING)")
+except Exception as e:
+    pass
+
+
